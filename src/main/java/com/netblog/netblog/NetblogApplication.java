1217734a7e4fa8cefd9275b6a8acbf0157f92690
Corey <75544957+TheHamhams@users.noreply.github.com>
2023-03-22 13:12:26 -0700

    Initial commit

---
+++ src/main/java/com/netblog/netblog/NetblogApplication.java
@@ -0,0 +1,13 @@
+package com.netblog.netblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netblog/netblog/NetblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netblog.netblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
