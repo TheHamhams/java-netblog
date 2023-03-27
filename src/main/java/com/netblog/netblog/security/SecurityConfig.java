5e2d78062447e12545460f718cc72a360db3d520
Corey <75544957+TheHamhams@users.noreply.github.com>
2023-03-27 13:23:38 -0700

    Added CorsFilter to SecurityConfig

--- src/main/java/com/netblog/netblog/security/SecurityConfig.java
+++ src/main/java/com/netblog/netblog/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.netblog.netblog.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -9,5 +11,12 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
